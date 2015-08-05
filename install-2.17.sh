f1ef848a6a279c9eb451cf75bdfa1916c899ea87
Barinov Igor <igorbarinov@me.com>
2015-08-05 13:33:57 -0700

    2.17

---
+++ install-2.17.sh
@@ -0,0 +1,4 @@
+wget http://support.illumina.com/content/dam/illumina-support/documents/downloads/software/bcl2fastq/bcl2fastq2-v2.17.1.14-Linux-x86_64.rpm
+sudo apt-get install alien
+sudo alien bcl2fastq2-v2.17.1.14-Linux-x86_64.rpm
+sudo dpkg -i bcl2fastq2_0v2.17.1.14-2_amd64.deb

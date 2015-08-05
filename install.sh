8bb6944acc0ecf25180b87a9a0de4c020d880103
Igor Barinov <igorbarinov@me.com>
2015-08-05 02:12:53 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo dpkg -i bcl2fastq_1.8.4-2_amd64_cut.deb
+
+curl -kL http://install.perlbrew.pl | bash
+echo >> ~/.bash_profile "source ~/perl5/perlbrew/etc/bashrc"
+perlbrew install perl-5.14.4
+perlbrew switch perl-5.14.4
+perlbrew install-cpanm
+cpanm XML/Simple.pm
+

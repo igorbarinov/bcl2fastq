7607b14168a41f90fdbdaa754b7612090b539af6
Igor Barinov <bis@hintsolutions.ru>
2015-08-08 21:36:50 -0700

    Update install-2.17.sh

--- install-2.17.sh
+++ install-2.17.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 wget http://support.illumina.com/content/dam/illumina-support/documents/downloads/software/bcl2fastq/bcl2fastq2-v2.17.1.14-Linux-x86_64.rpm
-sudo apt-get -i install alien --assume-yes
+sudo apt-get install alien --assume-yes
 sudo alien bcl2fastq2-v2.17.1.14-Linux-x86_64.rpm
 sudo dpkg -i bcl2fastq2_0v2.17.1.14-2_amd64.deb
